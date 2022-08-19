ebd5e7a83370f4d02b74cc0310dd21efca40a806
Monineath Sar <monineathsar@gmail.com>
2022-08-18 19:39:15 -0700

    creates mock data for users, blog posts, & comments

---
+++ seeds/blogPostData.js
@@ -0,0 +1,28 @@
+const { BlogPost } = require('../models');
+
+const blogPostData = [
+    [
+        {
+          title: "What are you thoughts on remote work?",
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus imperdiet nulla malesuada pellentesque elit eget gravida. Magna fringilla urna porttitor rhoncus. Eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Augue lacus viverra vitae congue eu consequat ac felis. Augue ut lectus arcu bibendum at varius vel. Id ornare arcu odio ut sem. Tellus cras adipiscing enim eu turpis egestas pretium aenean.",
+          date: 2022-08-20,
+          username: "SalLovesPizza_95"
+        },
+        {
+          title: "How much pay should I expect for a bootcamp graduate?",
+          content: "Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu non diam phasellus vestibulum lorem sed risus. At tellus at urna condimentum mattis pellentesque id nibh tortor. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Eget aliquet nibh praesent tristique magna sit. Elit duis tristique sollicitudin nibh sit amet commodo nulla. Dignissim enim sit amet venenatis urna. Nibh cras pulvinar mattis nunc sed.",
+          date: 2022-08-18,  
+          username: "Lernantino_81"
+        },
+        {
+          title: "Yearly bonus as a new hire",
+          content: "Eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Augue lacus viverra vitae congue eu consequat ac felis. Augue ut lectus arcu bibendum at varius vel. Id ornare arcu odio ut sem. Tellus cras adipiscing enim eu turpis egestas pretium aenean. Mauris a diam maecenas sed enim ut sem viverra aliquet. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu non diam phasellus vestibulum lorem sed risus. At tellus at urna condimentum mattis pellentesque id nibh tortor. ",
+          date: 2022-08-03 ,
+          username: "AmikoIsTheBest"
+        }
+      ]
+];
+
+const seedBlogData = () => BlogPost.bulkCreate(blogPostData);
+
+module.exports = seedBlogData;
---
+++ seeds/userData.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+const userData = [
+    [
+        {
+          username: "SalLovesPizza_95",
+          password: "password12345"
+        },
+        {
+          username: "Lernantino_81",
+          password: "password12345"
+        },
+        {
+          username: "AmikoIsTheBest",
+          password: "password12345"
+        }
+      ]
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;

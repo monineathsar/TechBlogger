fabec49c9d74f7e55dc9bde58c5a0d9bb32d1aaa
monineathsar <monineathsar@gmail.com>
2024-03-03 21:24:42 -0800

    added username and password empty validation

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
 const usernameInput = document.getElementById('userNameInput');
 const passwordInput = document.getElementById('passwordInput');
 const loginBtn = document.getElementById('loginBtn');
+const demoBtn = document.getElementById('demoBtn');
 
 // js for when user clicks log-in button
 loginBtn.addEventListener('click', async (event) => {
@@ -9,7 +10,12 @@ loginBtn.addEventListener('click', async (event) => {
         const username = usernameInput.value.trim();
         const password = passwordInput.value.trim();
         
-        const response = await fetch('/api/user/login', {
+        if (username ==='' || password ==='') {
+            alert('Username and password cannot be empty.');
+            return;
+        }
+
+        await fetch('/api/user/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -24,3 +30,27 @@ loginBtn.addEventListener('click', async (event) => {
         alert(error);
     }
 });
+
+// js for when user clicks demo button
+demoBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    try {
+        const username = "test";
+        const password = "password";
+        
+        await fetch('/api/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            })
+        });
+        window.location.href = '/'; 
+    } catch (error) {
+        alert(error);
+    }
+});
+
